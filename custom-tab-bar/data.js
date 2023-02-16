cf8fc5de07eb7aff460f2a6356562bbee0ef7be5
lichongbing <865143152@qq.com>
2023-02-16 09:05:30 +0800

    删除不用的页面
    删除商品，购物车，订单页面

--- custom-tab-bar/data.js
+++ custom-tab-bar/data.js
@@ -5,11 +5,6 @@ export default [
     url: 'pages/home/home',
   },
   {
-    icon: 'sort',
-    text: '分类',
-    url: 'pages/goods/category/index',
-  },
-  {
     icon: 'person',
     text: '个人中心',
     url: 'pages/usercenter/index',
